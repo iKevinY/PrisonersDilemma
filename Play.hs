5101c574001a103a1a03ae190ba90b35ec9f453c
vw511 <hq.vicky.w@gmail.com>
2016-11-30 15:05:12 -0800

    Added final score and possible scores
    Improved console;
    Added more score related results;
    Got rid or “who won”

--- Play.hs
+++ Play.hs
@@ -3,43 +3,51 @@ module Play where
 import PrisonersDilemma
 import System.IO
 
--- Use `play <opponent-type>` to start a game
-play opponent_type = person_play pd (pd Start) opponent_type
+-- Use `play <ai_strategy>` to start a game
+play ai_strategy = person_play pd (pd Start) ai_strategy
 
 -- opponent has played, the person must now play
-person_play game (EndOfGame 1 score) opponent =
-  do putStrLn "------------ Game Over --------------"
+person_play game (EndOfGame 1 score) ai_strategy =
+  do putStrLn "------------ *Game Over* --------------"
      putStrLn "Results:"
-     putStrLn "-> Computer won."
-     putStrLn("Score: " ++ (show score))
+     putStrLn("-> Final Score:  " ++ (show score) ++ " (highest possible group score: " ++ show (3*totalRounds,3*totalRounds) ++ ")")
+     putStrLn("-> Your score:     " ++ (show (snd score)) ++ "    (highest possible individual score: " ++ show (4*totalRounds) ++ ")")
+     putStrLn("-> Agent's score:  " ++ (show (fst score)) ++ "    (highest possible individual score: " ++ show (4*totalRounds) ++ ")")
 
-person_play game (EndOfGame 0 score) opponent =
-  do putStrLn "------------ Game Over --------------"
-     putStrLn "Result:"
-     putStrLn "-> It's a draw."
-     putStrLn("Score: " ++ (show score))
-
-person_play game (EndOfGame (-1) score) opponent =
-  do putStrLn "------------ Game Over --------------"
+person_play game (EndOfGame 0 score) ai_strategy =
+  do putStrLn "------------ *Game Over* --------------"
      putStrLn "Results:"
-     putStrLn "-> You won."
-     putStrLn("Score: " ++ (show score))
+     putStrLn("-> Final Score:  " ++ (show score) ++ " (highest possible group score: " ++ show (3*totalRounds,3*totalRounds) ++ ")")
+     putStrLn("-> Your score:     " ++ (show (snd score)) ++ "    (highest possible individual score: " ++ show (4*totalRounds) ++ ")")
+     putStrLn("-> Agent's score:  " ++ (show (fst score)) ++ "    (highest possible individual score: " ++ show (4*totalRounds) ++ ")")
 
-person_play game (ContinueGame state) opponent =
+person_play game (EndOfGame (-1) score) ai_strategy =
+  do putStrLn "------------ *Game Over* --------------"
+     putStrLn "Results:"
+     putStrLn("-> Final Score:  " ++ (show score) ++ " (highest possible group score: " ++ show (3*totalRounds,3*totalRounds) ++ ")")
+     putStrLn("-> Your score:     " ++ (show (snd score)) ++ "    (highest possible individual score: " ++ show (4*totalRounds) ++ ")")
+     putStrLn("-> Agent's score:  " ++ (show (fst score)) ++ "    (highest possible individual score: " ++ show (4*totalRounds) ++ ")")
+
+person_play game (ContinueGame state) ai_strategy =
   do
-    putStrLn("Your moves (n -> old): " ++ show (fst state))
-    putStrLn("Their moves:             " ++ show (snd state))
+    putStrLn("------------- *Round " ++ show (fromIntegral(length (fst state) + 1))++ "* --------------")
+    putStrLn("-> Your moves (recent -> late): " ++ show (fst state))
+    putStrLn("-> the agent's moves:           " ++ show (snd state))
     putStrLn("Pick a move... (1 = cooperate, 0 = defect)")
     line <- getLine
     if (read line :: Int) == 1
     then
-      computer_play game (game (Move C state)) opponent
+      computer_play game (game (Move C state)) ai_strategy
       else if (read line::Int) == 0 
-      then computer_play game (game (Move D state)) opponent
-        else do putStrLn "You had a typo. Type again."
-                person_play game (ContinueGame state) opponent
+      then computer_play game (game (Move D state)) ai_strategy
+        else do putStrLn "###########################################"
+                putStrLn "#                                         #"
+                putStrLn "#    You had a typo. Redo this round:     #"
+                putStrLn "#                                         #"
+                putStrLn "###########################################"
+                person_play game (ContinueGame state) ai_strategy
 
 
 -- person has played, the computer must now play
-computer_play game (ContinueGame state) opponent =
-  person_play game (game (Move (opponent state) state)) opponent
+computer_play game (ContinueGame state) ai_strategy =
+  person_play game (game (Move (ai_strategy state) state)) ai_strategy
